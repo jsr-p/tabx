e98b8da5b0ed0fa06c9817e650b78c2ccb5d7bfd
jsr-p <jsr-p@sodas.ku.dk>
2025-12-27 11:17:26 +0000

    Update text mod

--- src/tabx/text.py
+++ src/tabx/text.py
@@ -2,18 +2,22 @@ from typing import Literal
 
 
 def bf(s: str):
+    """Wrap text in LaTeX \\textbf{} (boldface)."""
     return rf"\textbf{{{s}}}"
 
 
 def tt(s: str):
+    """Wrap text in LaTeX \\texttt{} (typewriter / monospace)."""
     return rf"\texttt{{{s}}}"
 
 
 def it(s: str):
+    """Wrap text in LaTeX \\textit{} (italic)."""
     return rf"\textit{{{s}}}"
 
 
 def color(s: str, color: str):
+    """Wrap text in LaTeX \\textcolor{<color>}{}."""
     return rf"\textcolor{{{color}}}{{{s}}}"
 
 
@@ -27,3 +31,13 @@ def rotatebox(
     See https://latexref.xyz/_005crotatebox.html
     """
     return rf"\rotatebox[origin={origin}]{{{angle}}}{{{s}}}"
+
+
+def parentheses(s: str):
+    """Wrap string in parentheses"""
+    return f"({s})"
+
+
+def brackets(s: str):
+    """Wrap string in square brackets"""
+    return f"[{s}]"
